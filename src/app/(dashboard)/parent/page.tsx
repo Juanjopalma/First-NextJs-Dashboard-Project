4560663c0315d176e6e0149ca488e66e64f75e21
Juanjopalma <juanjose.palma@sdggroup.com>
2025-04-07 15:50:54 +0200

    parent component

--- src/app/(dashboard)/parent/page.tsx
+++ src/app/(dashboard)/parent/page.tsx
@@ -1,7 +1,23 @@
+import Announcements from '@/components/Announcements/Announcements';
+import BigCalendar from '@/components/BigCalendar/BigCalendar';
+import './parent.scss';
+
 
 const ParentPage = () => {
     return (
-        <div>ParentPage</div>
+        <div className='parent'>
+            {/* LEFT */}
+            <div className="parent__left">
+                <div className="parent__left-schedule">
+                    <h1>Schedule (John Doe)</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="parent__right">
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
