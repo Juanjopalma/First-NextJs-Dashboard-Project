d1445a9502a4eb5c73744d37a099b76ec5ce013d
Juanjopalma <juanjose.palma@sdggroup.com>
2025-04-07 15:47:15 +0200

    student and teacher components are done

--- src/app/(dashboard)/student/page.tsx
+++ src/app/(dashboard)/student/page.tsx
@@ -1,3 +1,6 @@
+import EventCalendar from '@/components/EventCalendar/EventCalendar';
+import Announcements from '@/components/Announcements/Announcements';
+import BigCalendar from '@/components/BigCalendar/BigCalendar';
 import './student.scss';
 
 
@@ -5,9 +8,17 @@ const StudentPage = () => {
     return (
         <div className='student'>
             {/* LEFT */}
-            <div className=""></div>
+            <div className="student__left">
+                <div className="student__left-schedule">
+                    <h1>Schedule (4A)</h1>
+                    <BigCalendar />
+                </div>
+            </div>
             {/* RIGHT */}
-            <div className=""></div>
+            <div className="student__right">
+                <EventCalendar />
+                <Announcements />
+            </div>
         </div>
     )
 }
--- src/app/(dashboard)/teacher/page.tsx
+++ src/app/(dashboard)/teacher/page.tsx
@@ -1,7 +1,23 @@
+import Announcements from '@/components/Announcements/Announcements';
+import BigCalendar from '@/components/BigCalendar/BigCalendar';
+import './teacher.scss';
+
 
 const TeacherPage = () => {
     return (
-        <div>TeacherPage</div>
+        <div className='teacher'>
+            {/* LEFT */}
+            <div className="teacher__left">
+                <div className="teacher__left-schedule">
+                    <h1>Schedule</h1>
+                    <BigCalendar />
+                </div>
+            </div>
+            {/* RIGHT */}
+            <div className="teacher__right">
+                <Announcements />
+            </div>
+        </div>
     )
 }
 
